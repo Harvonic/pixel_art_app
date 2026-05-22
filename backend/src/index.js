85970849121749e51e4d6254919ccdb705c5e49a
Harvonic <harvinchhabra@gmail.com>
2026-05-22 17:32:10 -0400

    Setup basic frontend and backend

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+    res.json({ message: "Backend running" });
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
